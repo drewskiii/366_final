328759af4542886433f42e981bfb8495c18e66a6
Grant Matejka <grantmatejka1@gmail.com>
2020-06-12 13:39:02 -0700

    Update IngredientTests.java

--- src/test/java/csc366/jpademo/IngredientTests.java
+++ src/test/java/csc366/jpademo/IngredientTests.java
@@ -36,7 +36,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 	"logging.pattern.console= %d{yyyy-MM-dd HH:mm:ss} - %msg%n"
 })
 @TestMethodOrder(OrderAnnotation.class)
-public class Demo5 {
+public class IngredientTests {
 	private final static Logger log = LoggerFactory.getLogger(Demo5.class);
 
 	@Autowired
